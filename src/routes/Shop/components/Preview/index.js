1634972786bef928255850a93f7e67749b6ee1f1
leiming8 <leiming8@wanda.cn>
2016-12-13 12:45:14 +0800

    Add react-grid-layout

--- src/routes/Shop/components/Preview/index.js
+++ src/routes/Shop/components/Preview/index.js
@@ -1,21 +1,40 @@
-import React, {Component, PropTypes} from 'react'
+import React, { Component, PropTypes } from 'react'
+import ReactGridLayout from 'react-grid-layout'
+
 import './Preview.scss'
 
-export class Preview extends Component{
+
+export class Preview extends Component {
 
   static propTypes = {
-    //onDrop: PropTypes.func.isRequired,
+    canDrop          : PropTypes.bool.isRequired,
+    isOver           : PropTypes.bool.isRequired,
+    connectDropTarget: PropTypes.func.isRequired,
   }
 
   render() {
-    //console.log(this.props)
-    //console.log("-----------")
-    //console.log(this.props.dispatch)
-    const { canDrop, isOver, connectDropTarget, onDrop } = this.props;
-    const isActive = canDrop && isOver;
-    return connectDropTarget(<div className="preview-container">
-      {JSON.stringify(this.props)}
-    </div>)
+    const { canDrop, isOver, connectDropTarget } = this.props
+    //const isActive = canDrop && isOver;
+
+    var layout = [
+      { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
+      { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: 'c', x: 4, y: 0, w: 1, h: 2 }
+    ];
+    return connectDropTarget(
+      <div>
+        <pre style={{ "fontSize": '14px' }}>
+          {JSON.stringify(this.props, null, 2)}
+        </pre>
+        <div className="preview-container">
+          <ReactGridLayout className="layout" layout={layout} cols={4} rowHeight={30} width={300}>
+            <div key={'a'}>a</div>
+            <div key={'b'}>b</div>
+            <div key={'c'}>c</div>
+          </ReactGridLayout>
+        </div>
+      </div>
+    )
   }
 
 }
