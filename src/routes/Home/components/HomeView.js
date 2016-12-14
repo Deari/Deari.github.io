8ed500800199e7415790023ebd8cc27e5034456a
v_zhangqingqing <cyanzhang09@163.com>
2016-12-14 15:54:21 +0800

    home style add

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -13,8 +13,8 @@ export const HomeView = () => (
 	    		<h5>我是开发者</h5>
     		</div>
     		<form>
-    			<input type="text/css" placeholder="用户名"/>
-    			<input type="text/css" placeholder="请输入密码"/>
+    			<input className="form-control" type="text/css" placeholder="用户名"/>
+    			<input className="form-control" type="text/css" placeholder="请输入密码"/>
     			<div>
     				<label>
     					<input type="radio"/>
@@ -27,22 +27,27 @@ export const HomeView = () => (
     	</div>
     </div>
 		<div className='home_reminder'>
-			<span className='reminder_m'>系统公告：蓝海BO开放平台关于处理“二元期权”类信息的公告</span>
-			<span>查看更多</span>
+			<div>
+  			<span>系统公告：蓝海BO开放平台关于处理“二元期权”类信息的公告<i></i></span>
+        <span>查看更多</span>
+			</div>
 		</div>
     <div className='home_f'>
       <Link to='/shop' activeClassName='route--active'>
-        商家中心
+        <i className="homef-business"></i>
+        <h3>商家中心</h3>
         <p>一种新的开放能力，可以在微信内被便捷地获取和传播，同时具有出色的使用体验。</p>
         <span>了解更多</span>
       </Link>
       <Link to='/developer' activeClassName='route--active'>
-        开发者中心
+        <i className="homef-developer"></i>
+        <h3>开发者中心</h3>
         <p>一种新的开放能力，可以在微信内被便捷地获取和传播，同时具有出色的使用体验。</p>
         <span>了解更多</span>
       </Link>
       <Link to='/open' activeClassName='route--active'>
-        开放市场
+        <i className="homef-openmarket"></i>
+        <h3>开放市场</h3>
         <p>一种新的开放能力，可以在微信内被便捷地获取和传播，同时具有出色的使用体验。</p>
         <span>了解更多</span>
       </Link>
