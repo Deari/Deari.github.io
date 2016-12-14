b37ae50733a1e75ba8c08443b878e598672867d7
ITrenhao <renhao10@wanda.cn>
2016-12-14 16:29:58 +0800

    fetch添加

--- src/routes/Developer/routes/Apps/List.js
+++ src/routes/Developer/routes/Apps/List.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
-
+import fetchUtil from '../../../utils/fetchUtil.js';
 class AppList extends React.Component {
+  // async componentDidMount() {
+  //   try {
+  //      alert()
+  //     const res = await fetchUtil.getJSON(`http://10.1.115.14:8006/bo/v1/web/app/7`);
+  //     console.log(res)
+  //   } catch (e) {
+  //     console.log(e)
+  //   }
+  // }
   render() {
     return <div>hello 我的应用</div>
   }
@@ -11,3 +20,5 @@ module.exports = {
   path: 'list',
   component: AppList
 }
+
+// /bo/v1/web/app/${appId}
