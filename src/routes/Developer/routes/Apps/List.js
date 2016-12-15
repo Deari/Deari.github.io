36ac560c4cd6c596b0ffb2e47384cb5efc8cca86
ITrenhao <renhao10@wanda.cn>
2016-12-15 11:12:44 +0800

    接口处理

--- src/routes/Developer/routes/Apps/List.js
+++ src/routes/Developer/routes/Apps/List.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import AppsList from '../../components/Apps/List';
-import fetch from '../../../../../fetch'
+import fetchUtil from '../../../utils/fetchUtil'
 
 class AppList extends React.Component {
   async componentDidMount() {
+    const apiUrl = `http://10.1.115.14:8006/bo/v1/web/developer/1/app`
     try {
-      const res = await fetch(`http://10.1.115.14:8006/bo/v1/web/developer/1/app`);
-       if (res.ok) {
-          const resp = await res.json();
-       }
-      console.log()
+      const res = await fetchUtil.postJSON(apiUrl);
+      console.log(res)
     } catch (e) {
       console.log(e)
     }
