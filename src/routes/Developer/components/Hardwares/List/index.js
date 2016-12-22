c2c6ce365133143ec6a7c17537657516455079be
liuchang71 <liuchang71@wanda.cn>
2016-12-22 17:41:57 +0800

    hardsware list

--- src/routes/Developer/components/Hardwares/List/index.js
+++ src/routes/Developer/components/Hardwares/List/index.js
@@ -11,13 +11,13 @@ class HardwaresList extends React.Component {
     }
   }
   async getList() {
-    const apiUrl = `http://api.intra.sit.ffan.net/bo/v1/web/hardware/${this.state.status}`;
+    const apiUrl = `http://api.intra.sit.ffan.net/bo/v1/web/hardware/myHardware/${this.state.status}`;
     try {
       const res = await fetchUtil.getJSON(apiUrl);
       if(res.status === 200){
         return res.data && res.data.list && res.data.list
       } else {
-        console.log("res ", res);
+        window.alert(res.msg);
       }
     } catch (e) {
       console.log(e)
@@ -42,6 +42,7 @@ class HardwaresList extends React.Component {
   }
   render() {
     let status = this.state.status;
+    let listData = this.state.listData;
     return (
       <div className="cContent clx">
         <div className="col-sm-2 col-md-2 navThird">
@@ -61,16 +62,22 @@ class HardwaresList extends React.Component {
             </Link>
             <div className="">
               <ul>
-                <li>
-                  <a href="javascript:;">
-                    产品名称： name
-                    <img src="" alt="LOGO" />
-                    产品PID： PID
-                    创建时间： createTime
-                    更新时间： updateTime
-                  </a>
-                  {status === 'unpublished' ? <Link to='/developer/hardware/edit/1'><button className="btn">继续</button></Link> : ''}
-                </li>
+              {
+                listData.map((item, index) => {
+                  return (
+                    <li>
+                      <a href="javascript:;">
+                        产品名称： {item.hardwareName}
+                        <img src={item.hardwareLogo} alt="LOGO" />
+                        产品PID： {item.hardwarePid}
+                        创建时间： {item.createTime}
+                        更新时间： {item.updateTime}
+                      </a>
+                      {status === 'unpublished' ? <Link to='/developer/hardware/edit/1'><button className="btn">继续</button></Link> : ''}
+                    </li>
+                  )
+                })
+              }
               </ul>
             </div>
           </div>
