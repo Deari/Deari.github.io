7eadc9abc0a54c9650591f1af012540a320bc607
v_zhangqingqing <cyanzhang09@163.com>
2016-12-26 18:48:26 +0800

    moified header style

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -9,17 +9,17 @@ export const Sidebar = ({location, hideHeader}) => {
     <div className='sidebar'>
       <a className="create-btn"><i className="iconfont icon-create"></i>创建新组件</a>
       <ul className="help-menu">
-        <li><i className="iconfont icon-application"></i>我的应用</li>
-        <li><i className="iconfont icon-file"></i>应用文档</li>
+        <li><a className="active"><i className="iconfont icon-application"></i>我的应用</a></li>
+        <li><a><i className="iconfont icon-file"></i>应用文档</a></li>
       </ul>
       <ul className="tag-list">
-        <li className="active"><i className="iconfont icon-label"></i>全部标签</li>
-        <li><i className="iconfont icon-market-common"></i>营销常用</li>
-        <li><i className="iconfont icon-member-management"></i>会员管理</li>
-        <li><i className="iconfont icon-data-analysis"></i>数据分析</li>
-        <li><i className="iconfont icon-trade"></i>交易应用</li>
-        <li><i className="iconfont icon-pay"></i>支付系统</li>
-        <li><i className="iconfont icon-logistics"></i>物流系统</li>
+        <li><a><i className="iconfont icon-label"></i>全部标签</a></li>
+        <li><a><i className="iconfont icon-market-common"></i>营销常用</a></li>
+        <li><a><i className="iconfont icon-member-management"></i>会员管理</a></li>
+        <li><a><i className="iconfont icon-data-analysis"></i>数据分析</a></li>
+        <li><a><i className="iconfont icon-trade"></i>交易应用</a></li>
+        <li><a><i className="iconfont icon-pay"></i>支付系统</a></li>
+        <li><a><i className="iconfont icon-logistics"></i>物流系统</a></li>
       </ul>
     </div>
   )
