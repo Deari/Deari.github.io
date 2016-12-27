e5d4e874b4c3a9c675dedaaf150e6466b3969c0e
v_zhangqingqing <cyanzhang09@163.com>
2016-12-27 16:44:28 +0800

    修改iconfont

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -14,12 +14,12 @@ export const Sidebar = ({location, hideHeader}) => {
       </ul>
       <ul className="tag-list">
         <li><a><i className="iconfont icon-label"></i>全部标签</a></li>
-        <li><a><i className="iconfont icon-market-common"></i>营销常用</a></li>
-        <li><a><i className="iconfont icon-member-management"></i>会员管理</a></li>
-        <li><a><i className="iconfont icon-data-analysis"></i>数据分析</a></li>
-        <li><a><i className="iconfont icon-trade"></i>交易应用</a></li>
-        <li><a><i className="iconfont icon-pay"></i>支付系统</a></li>
-        <li><a><i className="iconfont icon-logistics"></i>物流系统</a></li>
+        <li><a><i className="iconfont icon-sidebar7"></i>营销常用</a></li>
+        <li><a><i className="iconfont icon-sidebar1"></i>会员管理</a></li>
+        <li><a><i className="iconfont icon-sidebar2"></i>数据分析</a></li>
+        <li><a><i className="iconfont icon-sidebar3"></i>交易应用</a></li>
+        <li><a><i className="iconfont icon-sidebar4"></i>支付系统</a></li>
+        <li><a><i className="iconfont icon-sidebar5"></i>物流系统</a></li>
       </ul>
     </div>
   )
