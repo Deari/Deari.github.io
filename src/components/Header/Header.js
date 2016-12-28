23404c3de71bb71bfe15129c144e5e83fec4c336
zhangzhiming15 <zhangzhiming15@wanda.cn>
2016-12-28 11:23:55 +0800

    move apps

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,44 +6,7 @@ import {ShopNav, DevNav, OpenNav} from './Navigation'
 import '../../styles/_base.scss'
 import '../../styles/iconfont/iconfont.css'
 
-const NavRules = [
-  {
-    title: '商家中心',
-    pathRe: new RegExp('^\\/shop\\/?'),
-    navC: <ShopNav />,
-    titleC: <Link to='/shop'>商家中心</Link>
-  },
-  {
-    title: '开放市场',
-    pathRe: new RegExp('^\\/open\\/?'),
-    navC: <OpenNav />,
-    titleC: <Link to='/open'>开放市场</Link>
-  },
-  {
-    title: '开发者中心',
-    pathRe: new RegExp('^\\/developer\\/\\w+\\/?'),
-    navC: <DevNav/>,
-    titleC: <Link to='/developer'>开发者中心</Link>
-  },
-  {
-    title: '开发者中心',
-    pathRe: new RegExp('^\\/developer\\/?$'),
-    titleC: <Link to='/developer'>开发者中心</Link>
-  }
-];
-
 export const Header = ({location, hideHeader}) => {
-
-  if (hideHeader) return <div></div>
-  const path = location.pathname;
-  let nav, title;
-
-  NavRules.map(({pathRe, navC, titleC})=>{
-    if(path.search(pathRe) > -1) {
-      nav = navC;
-      title = titleC;
-    }
-  })
 
   return (
     <div className='header-wrapper'>
@@ -56,20 +19,31 @@ export const Header = ({location, hideHeader}) => {
             <span className="text">BO开放平台</span>
 
           </Link>
-          {title ? <small className="small-title">{title}</small> : null}
+          <small className="small-title">开发者平台</small>
         </h1>
         
         <div className="pull-right">
-          <div className="nav item">
-            { nav }
+          <div className="nav">
+            <div className="nav-list">
+              <Link to='/apps' activeClassName='active'>
+                应用市场
+              </Link>
+              <Link to='/widgets' activeClassName='active'>
+                组件市场
+              </Link>
+              <Link to='/api' activeClassName='active'>
+                API
+              </Link>
+              <Link to='/hardware' activeClassName='active'>
+                硬件市场
+              </Link>
+            </div>
           </div>
 
-          { title ? <div className="search">
-              <i className="iconfont icon-search"></i>
-              <input type="text" placeholder="搜索API" />
-            </div>
-            : null
-          }
+          <div className="search">
+            <i className="iconfont icon-search"></i>
+            <input type="text" placeholder="搜索API" />
+          </div>
           <Login />
         </div>
       </div>
