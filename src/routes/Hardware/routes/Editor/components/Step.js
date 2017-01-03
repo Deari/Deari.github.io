addf31087a8a200c1925773fa602ee9085158b5b
zhangzhiming15 <zhangzhiming15@wanda.cn>
2017-01-03 22:02:11 +0800

    ADd

---
+++ src/routes/Hardware/routes/Editor/components/Step.js
@@ -0,0 +1,24 @@
+import './step.scss'
+
+const Step = (props)=>(
+  <div className="step">
+  	<h2>创建硬件</h2>
+  	<div className='branchBox'>
+	    <div className={props.page == 1? 'branch active':'branch'}>
+	    	<span>1</span>
+	    	<span>填写基本信息</span>
+	    </div>
+	    <div className="branchBox-line"></div>
+	    <div className={props.page == 2? 'branch active':'branch'}>
+	    	<span>2</span>
+	    	<span>填写平台信息</span>
+	    </div>
+	    <div className="branchBox-line"></div>
+	    <div className={props.page == 3? 'branch active':'branch'}>
+	    	<span>3</span>
+	    	<span>提交成功</span>
+	    </div>
+  	</div>
+  </div>
+)
+export default Step;
