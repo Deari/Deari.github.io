f158b7a3e4e301a0f6b51e6b545f2da611f03b39
v_xudanna <635212049@qq.com>
2017-01-04 16:08:35 +0800

    header、silderbar文字修改

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -12,7 +12,7 @@ class Sidebar extends React.Component {
     const type = this.props.type || "应用"
     return (
       <div className='sidebar'>
-        <Link className="create-btn" to={urls.create.url}><i className="iconfont icon-create"></i>{'创建新'+type}</Link>
+        <Link className="create-btn" to={urls.create.url}><i className="iconfont icon-create"></i>{'发布新'+type}</Link>
         <ul className="help-menu">
           <li><Link to={urls.list.url} className={(urls.list.active && 'active') || ''}><i className="iconfont icon-application"></i>{'我的'+ type }</Link></li>
           <li><Link to={urls.doc.url} className={(urls.doc.active && 'active') || ''}><i className="iconfont icon-file"></i>开发者文档</Link></li>
