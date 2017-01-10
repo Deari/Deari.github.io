b99314263117672cd3bf101d5e4b06b98269e13c
ITrenhao <renhao10@wanda.cn>
2017-01-10 20:17:31 +0800

    dev-new

--- src/routes/Hardware/routes/Create/containers/CreateContainer.js
+++ src/routes/Hardware/routes/Create/containers/CreateContainer.js
@@ -135,7 +135,7 @@ class CreateContainer extends Component {
     const { page } = this.props.hardwareCreate;
 
     const urls = {
-      create: { url: `/hardware/create`, name: '创建新硬件' },
+      create: { url: `/hardware/create`, name: '发布新硬件' },
       list: { url: `/hardware/list`, name: '我的硬件' },
       doc: { url: `/hardware/doc` }
     }
