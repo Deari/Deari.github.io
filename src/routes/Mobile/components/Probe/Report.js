2fe5ba8cff71aee059a54fd8fb21948e1c225287
zhangzhiming15 <zhangzhiming15@wanda.cn>
2017-01-10 21:42:59 +0800

    dev

--- src/routes/Mobile/components/Probe/Report.js
+++ src/routes/Mobile/components/Probe/Report.js
@@ -39,7 +39,7 @@ const ChartContainer = ({ allNum, data }) => {
         <defs>
           <linearGradient id="colorNum" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#2692fb" stopOpacity={0.8}/>
-            <stop offset="95%" stopColor="#2692fb" stopOpacity={0}/>
+            <stop offset="95%" stopColor="#2692fb" stopOpacity={0.1}/>
           </linearGradient>
         </defs>
       <XAxis dataKey="time"/>
