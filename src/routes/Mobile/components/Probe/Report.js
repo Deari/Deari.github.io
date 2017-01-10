bc543e86f06e42dea29f94dd91a7bebb14605b0d
zhangzhiming15 <zhangzhiming15@wanda.cn>
2017-01-10 17:29:05 +0800

    dev

--- src/routes/Mobile/components/Probe/Report.js
+++ src/routes/Mobile/components/Probe/Report.js
@@ -13,7 +13,7 @@ const ChartContainer = ({ data }) => {
   return <div>
     <h3>客流量分布图</h3>
     <div className="chart-container">
-      <AreaChart width={600} height={300} data={data}
+      <AreaChart width={1200} height={300} data={data}
           margin={{top: 5, right: 30, left: 20, bottom: 5}}>
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
@@ -25,12 +25,13 @@ const ChartContainer = ({ data }) => {
             <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
           </linearGradient>
         </defs>
-      <XAxis dataKey="name"/>
+      <XAxis dataKey="time"/>
       <YAxis/>
       <CartesianGrid strokeDasharray="3 3"/>
       <Tooltip/>
       <Legend />
-        <Area label={<CustomizedLabel />} type="linear" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+        <Area label={<CustomizedLabel />} type="linear" dataKey="num" 
+          stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
       </AreaChart>
     </div>
   </div>
@@ -47,13 +48,90 @@ const NavBar = (props) => {
   </ul>
 }
 
+const keyMap = {
+  am: [
+    '00:00~08:00',
+    '08:00~09:00',
+    '09:00~10:00',
+    '10:00~11:00',
+    '11:00~12:00'
+  ],
+  pm: [
+    '12:00~13:00',
+    '13:00~14:00',
+    '14:00~15:00',
+    '15:00~16:00',
+    '16:00~17:00'
+  ],
+  night: [
+    '17:00~18:00',
+    '18:00~19:00',
+    '19:00~20:00',
+    '20:00~21:00',
+    '21:00~24:00'
+  ]
+}
+
+const addTextKey = (source, key) => {
+  return source[key].map((v, i) => Object.assign({}, v, {
+      text: keyMap[key] && keyMap[key][i]
+    })
+  )
+}
+
 export class ProbeReport extends Component {
+  
+  getChartData = (source) => {
+    let zero2eight = 0;
+    const chartData = source.filter((v, i)=>{
+      if(i < 8) {
+        zero2eight += v.num;
+        return false;
+      }
+
+      if(i > 18) {
+        return false
+      }
+      
+      return true
+    })
+
+    chartData.unshift({
+      name: '0',
+      num: zero2eight
+    })
+
+    return chartData
+  }
+
+  getListData = (source) => {
+
+    const listData = {
+      am: source.slice(7, 11),
+      pm: source.slice(11, 16),
+      night: source.slice(16, 20)
+    }
+
+    listData.am.unshift({
+      num: source.slice(0, 8).reduce((num, cur)=> num+cur.num, 0)
+    })
+
+    listData.night.push({
+      num: source.slice(20).reduce((num, cur)=> num+cur.num, 0)
+    })
+
+    for(let key in listData) {
+      listData[key] = addTextKey(listData, key)
+    }
+    return listData;
+  }
+
   render() {
     const {data} = this.props;
     return <div>
       <NavBar />
-      <ChartContainer data={data} />
-      <AnalysisContainer />
+      <ChartContainer data={this.getChartData(data)} />
+      <AnalysisContainer data={this.getListData(data)} />
     </div>
   }
 }
--- src/routes/Mobile/containers/ProbeReportContainer.js
+++ src/routes/Mobile/containers/ProbeReportContainer.js
@@ -2,26 +2,39 @@ import React from 'react'
 
 import ProbeReport from '../components/Probe/Report'
 
+import fetchUtil from '../../utils/fetchUtil'
+import Debug from '../../utils/debug'
+
+import DATA from '../components/Probe/data'
+
 const promised = {
 }
 
 export const Promised = (promiseProp, Wrapped) => class extends React.Component {
   state = {
     data: [
-      { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-      { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-      { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-      { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-      { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-      { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-      { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+      
     ]
   }
   componentWillMount() {
+    const apiUrl = 'http://api.sit.ffan.com/bo/store/v1/storePersonSum?storeId=111&startTime=111&endTime=111';
     
+    fetchUtil.getJSON(apiUrl).then(res=>{
+      if(res.status == 200) {
+        console.info(res.data);
+        this.setState(res.data)
+      } else {
+        Debug.warn('获取数据异常', res);
+        this.setState({ data : DATA})
+      }
+    }).catch(e=>{
+      // Debug.warn('获取数据异常', e);
+      this.setState({ data : DATA})
+    })
   }
+
   render () {
-    const { data } = this.state;
+    const data = this.state.data;
     return <Wrapped data={data}/>
   }
 }
