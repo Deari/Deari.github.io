7c497f099354a33dfb8eb7a520adfb0ede119709
liuchang71 <liuchang71@wanda.cn>
2017-01-13 16:31:50 +0800

    domain

--- src/routes/Apps/components/Main.js
+++ src/routes/Apps/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import fetchUtil from 'routes/utils/fetchUtil'
-import { getDomain } from 'routes/utils/domain'
+import { getDomain } from 'utils/domain'
 import debug from 'routes/utils/debug'
 import Sidebar from 'components/Sidebar'
 import OpenList from 'components/OpenList'
@@ -19,8 +19,9 @@ class Main extends React.Component {
   }
 
   async getList(tagId) {
+
     let id = tagId || 'all';
-    let apiUrl = getDomain(`http://api.intra.`, `ffan.net/bo/v1/web/market/tag/${id}/apps`);
+    let apiUrl = getDomain(`web/market/tag/${id}/apps`);
     try {
       let res = await fetchUtil.getJSON(apiUrl)
       if (res.status === 200) {
@@ -34,7 +35,7 @@ class Main extends React.Component {
   }
 
   async getTags() {
-    let apiUrl = getDomain(`http://api.intra.`, `ffan.net/bo/v1/public/common/tags?type=app`);
+    let apiUrl = getDomain(`public/common/tags?type=app`);
     try {
       let res = await fetchUtil.getJSON(apiUrl)
       if (res.status === 200) {
