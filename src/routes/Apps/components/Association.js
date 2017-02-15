81d924059640bd942514cf3411cd6b1a6cf14ef9
ITrenhao <renhao10@wanda.cn>
2017-02-15 09:13:48 +0800

    h5create

--- src/routes/Apps/components/Association.js
+++ src/routes/Apps/components/Association.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react'
-alert(11111)
 import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 const defaultMoule = (props) => (
