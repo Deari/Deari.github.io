17cb6f9992c7b3ba7f6911bc3d1657bd6798046c
ITrenhao <renhao10@wanda.cn>
2017-03-01 13:51:34 +0800

    styleFixed

--- src/routes/Apps/routes/Create/components/ChoiceStep.js
+++ src/routes/Apps/routes/Create/components/ChoiceStep.js
@@ -14,7 +14,7 @@ class ChoiceStepForm extends Component {
     return (
       <form className="create-choice-block">
         <p className="create-choice-title">发布新应用</p>
-        <div className="create-form-btn">
+        <div className="createApp-form-btn">
           <h3 className="title">请选择应用类型</h3>
           <button type="button" className="next rn-btn" onClick={this.handleSubmit.bind(this, 0)}><i className="iconfont icon-facebook"></i>RN</button>
           <button type="button" className="next h5-btn" onClick={this.handleSubmit.bind(this, 1)}><i className="iconfont icon-html5"></i>HTML5</button>
