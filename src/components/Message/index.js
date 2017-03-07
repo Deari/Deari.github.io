50ae066b5d5c96b8f26461c5c68996fbcbb8376c
leiming8 <leiming8@wanda.cn>
2017-03-08 07:01:17 +0800

    Init finish message

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,16 +1,40 @@
-import React, { PropTypes, Component } from 'react'
+import React, { PropTypes, Component, PureComponent } from 'react'
 import classnames from 'classnames'
-
+const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup
 import {
   render,
   findDOMNode,
   unmountComponentAtNode,
   unstable_renderSubtreeIntoContainer as renderSubtreeIntoContainer
 } from 'react-dom'
+import DelayRenderer from 'components/Protal/DelayRenderer'
 
-const Message = ({children}) => <div>
-  {children}
-</div>
+// http://stackoverflow.com/questions/29884848/react-animation-not-working
+
+
+const LeaveTimeout = 1000
+
+class Message extends Component {
+  render() {
+
+    // TODO remove 时机
+    const items = this.props.items.map((item, i) => (
+      <div key={item} onClick={() => this.props.handleRemove(i)}>
+        {item}
+      </div>
+    ));
+
+    return (
+      <ReactCSSTransitionGroup
+        transitionName="example"
+        transitionAppear={true}
+        transitionAppearTimeout={1000}
+        transitionLeaveTimeout={LeaveTimeout}>
+        {items}
+      </ReactCSSTransitionGroup>
+    );
+  }
+}
 
 export class Messages {
 
@@ -28,15 +52,29 @@ export class Messages {
     return this._container
   }
 
+  remove(messageContainer) {
+
+    render(<Message items={[]}/>, messageContainer, () => {
+      console.log(3333333333)
+      setTimeout(()=> {
+        console.log(4444444444)
+        unmountComponentAtNode(messageContainer)
+        this._getContainer().removeChild(messageContainer)
+      }, LeaveTimeout)
+
+    })
+  }
+
   add(message, during) {
     const messageContainer = document.createElement('div')
     this._getContainer().appendChild(messageContainer)
-    render(<Message children={message}/>, messageContainer, () => {
-    //renderSubtreeIntoContainer(this._context, message, messageContainer, () => {
+    render(<Message items={[message]} />, messageContainer, () => {
+        //renderSubtreeIntoContainer(this._context, message, messageContainer, () => {
         if (during > 0) {
+          console.log(11111111)
           setTimeout(() => {
-            unmountComponentAtNode(messageContainer)
-            this._getContainer().removeChild(messageContainer)
+            console.log(2222222222)
+            this.remove(messageContainer)
           }, during)
         }
       }
--- src/routes/Shop/routes/ProductCreator/components/ProductCreator/index.js
+++ src/routes/Shop/routes/ProductCreator/components/ProductCreator/index.js
@@ -3,27 +3,79 @@ import './ProductCreator.scss'
 import Board from './Board'
 import { Accordion, Collapse } from 'components/Accordion'
 import { Messages } from 'components/Message'
+const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup
+
+class Todo extends Component {
+  render() {
+    const items = this.props.items.map((item, i) => (
+      <div key={item} onClick={() => this.props.handleRemove(i)}>
+        {item}
+      </div>
+    ));
+
+    return (
+      <ReactCSSTransitionGroup
+        transitionName="example"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+        {items}
+      </ReactCSSTransitionGroup>
+    );
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [ 'hello', 'world', 'click', 'me' ] };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    const newItems = this.state.items.concat([
+      prompt('Enter some text')
+    ]);
+    this.setState({ items: newItems });
+  }
+
+  handleRemove(i) {
+    let newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({ items: newItems });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <button onClick={this.handleAdd}>Add Item</button>
+        <Todo items={this.state.items} handleRemove={::this.handleRemove}/>
+      </div>
+    );
+  }
+}
 
 export class ProductCreator extends Component {
 
   constructor(props) {
     super(props)
-    this._messages = new Messages(this)
+    this._messages = new Messages()
   }
 
   onClick() {
-    this._messages.add('ttttttttttt', 3000)
+    this._messages.add(<div>sdfs</div>, 3000)
   }
 
   render() {
-    const mess = new Messages(this)
-    mess.add(<div>dddddddd</div>)
-    mess.add(<div>dcccf</div>, 3000)
-    mess.add(<div>ccccc</div>, 4000)
-    mess.add(<div>ddddd</div>, 5000)
+    const mess = new Messages()
+    //mess.add(<div>dddddddd</div>)
+    //mess.add(<div>dcccf</div>, 3000)
+    //mess.add(<div>ccccc</div>, 4000)
+    //mess.add(<div>ddddd</div>, 5000)
 
     return <div>
       <Board knightPosition={[ 3, 5 ]}/>
+      <TodoList/>
       <button onClick={::this.onClick}>Mess</button>
     </div>
   }
