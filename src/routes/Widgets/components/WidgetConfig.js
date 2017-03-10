76b39f487dcdf4186c1dd59d59cafa311bf3e186
v_chaoxinni <chaoxinni@163.com>
2017-03-10 17:01:07 +0800

    组件文件css

--- src/routes/Widgets/components/WidgetConfig.js
+++ src/routes/Widgets/components/WidgetConfig.js
@@ -7,16 +7,22 @@ import './widgetConfig.scss'
 class ConfigTpl extends Component {
   render(){
     return (<div className='config-box'>
-       <Field name="type" component={renderSelect}>
-          <option value="input">文本框</option>
-       </Field>
-       <div className='config-textBox'>
-       <Field name="label"  placeholder="标题" component={renderField}></Field>
-       <Field name="id"  placeholder="KEY" component={renderField}></Field>
-       <Field name="value"  placeholder="VALUE" component={renderField}></Field>
-       <Field name="desc"  placeholder="描述" component={renderField}></Field>
-       </div>
-    </div>)
+    					<div className="config-contain">
+    						<div className="config-btn"></div>
+    						<div className="config-clx"></div>
+				        <Field name="type" component={renderSelect}>
+				          <option value="input">文本框</option>
+				        </Field>
+				        <div className='config-textBox'>
+					        <Field name="label"  placeholder="标题" component={renderField}></Field>
+					        <Field name="id"  placeholder="KEY" component={renderField}></Field>
+					        <Field name="value"  placeholder="VALUE" component={renderField}></Field>
+					        <Field name="desc"  placeholder="描述" component={renderField}></Field>
+       					</div>
+       					<div className="config-clx"></div>
+       					<div className="config-edit">填写属性值</div>
+			       </div>
+			    </div>)
   }
 }
 const mapDispatchToProps = {
