7aa213798dd35cdabbefe07feb6856f6ffc187ac
ITrenhao <renhao10@wanda.cn>
2017-03-31 16:15:41 +0800

    wenan

--- src/routes/Apps/components/Complete.js
+++ src/routes/Apps/components/Complete.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 const Complete = (props)=>(
   <div className="step-finish1">
     <div className="finish-btn">
-      <p><i className="iconfont icon-finish"></i><span>成功创建应用</span></p>
+      <p><i className="iconfont icon-finish"></i><span>提交成功，等待审核</span></p>
     </div>
     <div className="finish-text-box">
       <p>审核大概需要1周时间,请耐心等待,审核完成后,会以邮件的形式告知您,请您关注登录平台时的邮箱。或者您可以通过发送邮件,到bo@wanda.com.cn查询审核结果。</p>
