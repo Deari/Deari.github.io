d83af620fef4bacaeb1bd5dcb457e11bb671dc88
ITrenhao <renhao10@wanda.cn>
2017-04-07 16:08:20 +0800

    search

--- src/components/ListNav/index.js
+++ src/components/ListNav/index.js
@@ -17,9 +17,12 @@ class ListNav extends React.Component {
     const {navData, handleSearch, label, searchValue} = this.props
     const str = ''
     return (
-      <div>
-      {searchValue?<i onClick={str=>{handleSearch(str)}}>x</i>:''}
-        <input type="text" placeholder={label}  onChange={e=>{handleSearch(e)}} value={searchValue?searchValue:''}/>
+      <div className="list-header-nav">
+        <div className="list-header-search">
+          {searchValue?<i className="iconfont icon-close" onClick={str=>{handleSearch(str)}}></i>:''}
+           <i className="iconfont icon-search"></i>
+          <input type="text" placeholder={label}  onChange={e=>{handleSearch(e)}} value={searchValue?searchValue:''}/>
+        </div>
         <BasicNav navData={navData} onChange={this.changeNav.bind(this)} />
       </div>
      
