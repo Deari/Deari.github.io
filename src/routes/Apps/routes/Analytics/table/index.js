0969e677835bb34d5692bf325aba98908e86ca66
v_chaoxinni <chaoxinni@163.com>
2017-04-19 17:00:11 +0800

    data css

--- src/routes/Apps/routes/Analytics/table/index.js
+++ src/routes/Apps/routes/Analytics/table/index.js
@@ -5,11 +5,12 @@ class Table extends Component {
   
   render() {
     return (
-      <div className="panel">
-        <table>
+     <div class="panel">
+        <h2><i class="iconfont icon-data-count"></i>应用数据统计</h2>
+        <table cellspacing="0" cellpadding="0">
           <thead>
             <tr>
-              <th>LOGO</th>
+              <th>Logo</th>
               <th>应用名称</th>
               <th>新增商家</th>
               <th>活跃商家</th>
@@ -24,25 +25,25 @@ class Table extends Component {
               <td>
                 <div>
                   <span>会员管理</span>
-                  <span>FAP</span>
-                  <span>简介</span>
+                  <span>全国30多个省市县的邮编号码查询，数据权威准确，数百万条数据, 全国30多个省市县的邮编号码查询，数据权威准确，数百万条数据</span>
+                  <span>在 应用市场 中查看<i class="iconfont icon-look"></i></span>
                 </div>
               </td>
               <td>
-                <span>今：2</span>
-                <span>昨：2</span>
+                <span>今：<i class="num-color">2</i></span>
+                <span>昨：<i class="num-color">2</i></span>
               </td>
               <td>
-                <span>今：2</span>
-                <span>昨：2</span>
+                <span>今：<i class="num-color">2</i></span>
+                <span>昨：<i class="num-color">2</i></span>
               </td>
               <td>
-                <span>今：2</span>
-                <span>昨：2</span>
+                <span>今：<i class="num-color">2</i></span>
+                <span>昨：<i class="num-color">2</i></span>
               </td>
               <td>
-                <span>今：2</span>
-                <span>昨：2</span>
+                <span>今：<i class="num-color">2</i></span>
+                <span>昨：<i class="num-color">2</i></span>
               </td>
               <td>
                 <span>查看</span>
