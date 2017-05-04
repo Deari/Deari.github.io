5abae9ab2c74c2ba63e9449baeb062ad1fc65d4e
zhangzhiming15 <zhangzhiming15@wanda.cn>
2017-05-04 14:30:13 +0800

    add demo

--- src/routes/Demo/index.js
+++ src/routes/Demo/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Test = ()=>(<div>This is a Test.</div>);
 
 export default (store) => ({
@@ -7,28 +6,12 @@ export default (store) => ({
     component: Test
   },
 
-=======
-export default (store) => ({
-  path: 'demo',
-  indexRoute: {
-    getComponents(nextState, cb) {
-      require.ensure([], (require) => {
-        cb(null, {
-          children: require('./components/A').default
-        })
-      })
-    }
-  },
->>>>>>> master
+
   getChildRoutes(partialNextState, cb) {
     require.ensure([], (require) => {
       cb(null, [
         require('./routes/1-2').default,
-<<<<<<< HEAD
-        require('./routes/ProductCreator').default
-=======
-        require('./routes/ProductCreator')(store),
->>>>>>> master
+        require('./routes/ProductCreator').default,
         require('./routes/Counter')(store)
       ])
     })
