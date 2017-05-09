1e670b448bb0bc8d34ebdbd5b702ce3762d8c6c3
zhangzhiming15 <zhangzhiming15@wanda.cn>
2017-05-09 15:53:40 +0800

    add

--- src/config/index.js
+++ src/config/index.js
@@ -5,12 +5,12 @@ export const PageTypes = {
 }
 
 export const DocLink = (type) => {
-  let docLink = (type == 'hardware') ? `http://iotdev.ffan.net/zh-cn/developer/document/1` : `/${type}/doc`
+  let docLink = (type === 'hardware') ? `http://iotdev.ffan.net/zh-cn/developer/document/1` : `/${type}/doc`
   return docLink
 }
 
 export const MyLink = (type) => {
-  let myLink = (type == 'hardware') ? `http://iotdev.ffan.net/zh-cn/developer/product/all` : `/${type}/list`
+  let myLink = (type === 'hardware') ? `http://iotdev.ffan.net/zh-cn/developer/product/all` : `/${type}/list`
   return myLink
 }
 
@@ -19,16 +19,12 @@ export const getPageLinks = (type) => {
     to: MyLink(type),
     label: `我的${PageTypes[type]}`,
     icon: 'application',
-    isExternal: (type == 'hardware') ? true : false
+    isExternal: type === 'hardware'
   },{
     to: `/${type}/analytics`,
     label: `${PageTypes[type]}数据统计`,
     icon: 'sidebar3',
-    hide: (type == 'hardware') ? true : false
-  },{
-    to: `/applyAccount`,
-    label: '申请测试账号',
-    icon: 'application',
+    hide: type === 'hardware'
   },{
     to: `/devtools`,
     label: '开发者工具',
@@ -37,7 +33,7 @@ export const getPageLinks = (type) => {
     to: DocLink(type),
     label: '开发者文档',
     icon: 'file',
-    isExternal: (type == 'hardware') ? true : false
+    isExternal: type === 'hardware'
   }]
 }
 
