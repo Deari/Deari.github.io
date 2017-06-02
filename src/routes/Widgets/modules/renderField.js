11339719f97ea7e0c2d928e1fee8f19d4a353a5d
zhangzhiming15 <zhangzhiming15@wanda.cn>
2017-06-02 14:53:49 +0800

    update url

--- src/routes/Widgets/modules/renderField.js
+++ src/routes/Widgets/modules/renderField.js
@@ -207,8 +207,8 @@ export class renderFile extends Component {
           
           { isMiniProgram ? <div className='mini-program-xx'>
             应用类型为“FAP小程序”，请您先使用打包工具(
-              <a style={{color: '#2692fb', textDecoration: 'underline'}} href='http://fdfs.ffan.net/v2/file/nDjvYxof9gD9AjbH9o4al93o5T2mw9Yp?attachExt=2'>Windows</a> 
-              | <a  style={{color: '#2692fb', textDecoration: 'underline'}} href='http://fdfs.ffan.net/v2/file/BgbBeXayRXw6H5h4aF3115Egr50dM6pr?attachExt=2'>Mac</a>
+              <a style={{color: '#2692fb', textDecoration: 'underline'}} href='http://fdfs.ffan.net/v2/file/3l8v0ztIcPI9vBB18g614JF370J47byY?attachExt=2'>Windows</a> 
+              | <a  style={{color: '#2692fb', textDecoration: 'underline'}} href='http://fdfs.ffan.net/v2/file/ZnFWmHtPYah2Fq2a8nuGhVW1NCT9OTMV?attachExt=2'>Mac</a>
               )进行打包，请将打包完成后的应用进行上传。
           </div> : null }
           <span className='right-upload'>
